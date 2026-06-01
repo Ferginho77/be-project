4466260bf799a87b2ef846a1d7ecebf1ae8021e0
Ferginho77 <fergiefakhirusalam@gmail.com>
2026-06-01 15:23:01 +0700

    hm

--- config/db.go
+++ config/db.go
@@ -1,3 +1,6 @@
+// CONFIG ONLINE
+
+
 package config
 
 import (
@@ -8,16 +11,60 @@ import (
 )
 
 var DB *gorm.DB
+var mysqlDB = require("mysql")
+
+
+const (
+	host     = "xu1nvg.h.filess.io"
+	port     = "3307"
+	user     = "goland_settingeat"
+	password = "edc5b3d3d8bfc6bc05b7a052bc24905752e21aa1"
+	dbname   = "goland_settingeat"
+)
 
 func Conn() {
-	dsn := "root:@tcp(127.0.0.1:3306)/goland?parseTime=true"
-	var err error
+	uri := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbname)
 
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := sql.Open("mysql", uri)
+	defer db.Close()
 
 	if err != nil {
-		log.Fatal("Failed to connect to database: ", err)
-	} else {
-		fmt.Println("Database connection successful")
+		log.Fatal(err)
 	}
+
+	var version string
+
+	err2 := db.QueryRow("SELECT VERSION()").Scan(&version)
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	fmt.Println(version)
 }
+
+// CONFIG LOCAL
+
+// package config
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// )
+
+// var DB *gorm.DB
+
+// func Conn() {
+// 	dsn := "root:@tcp(127.0.0.1:3306)/goland?parseTime=true"
+// 	var err error
+
+// 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+// 	if err != nil {
+// 		log.Fatal("Failed to connect to database: ", err)
+// 	} else {
+// 		fmt.Println("Database connection successful")
+// 	}
+// }
--- main.go
+++ main.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-    "rest-api/config"
-    "github.com/gin-gonic/gin"
-    "github.com/gin-contrib/cors"
-    "rest-api/controllers"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+
+	"backend/config"
+	"backend/controllers"
 )
 
 func main() {
-    r := gin.Default()
+	r := gin.Default()
 
-    // Setup CORS agar bisa diakses oleh React (biasanya port 3000 atau 5173)
-    r.Use(cors.Default())
+	r.Use(cors.Default())
 
-    // Koneksi Database
-    config.Conn()
+	config.Conn()
 
-    // Route Get Semua Data
-    r.GET("/tanamans", controllers.GetTanaman)
-    r.GET("/lahans", controllers.GetLahan)
-    r.GET("/inventaris", controllers.GetInventaris)
-    r.GET("/schedulers", controllers.GetScheduler)
+	r.GET("/tanamans", controllers.GetTanaman)
+	r.GET("/lahans", controllers.GetLahan)
+	r.GET("/inventaris", controllers.GetInventaris)
+	r.GET("/schedulers", controllers.GetScheduler)
 
-    // Route Delete
-    r.DELETE("/lahans/:id", controllers.DeleteLahan)
-    r.DELETE("/inventaris/:id", controllers.DeleteInventaris)
+	r.DELETE("/lahans/:id", controllers.DeleteLahan)
+	r.DELETE("/inventaris/:id", controllers.DeleteInventaris)
 
-    // Route Tambah Data
-    r.POST("/schedulers", controllers.TambahScheduler)
+	r.POST("/schedulers", controllers.TambahScheduler)
 
-    // Route Update Status
-    r.PUT("/schedulers/:id/status", controllers.UpdateStatus)
+	r.PUT("/schedulers/:id/status", controllers.UpdateStatus)
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080" // untuk development lokal
+	}
 
-    r.Run(":8080") // Default di port 8080
+	r.Run(":" + port)
 }
