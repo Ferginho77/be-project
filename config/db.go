0f332697da889b58b2e9fc133e2101e89895549f
Ferginho77 <fergiefakhirusalam@gmail.com>
2026-07-08 00:27:09 +0700

    update services

--- config/db.go
+++ config/db.go
@@ -1,13 +1,13 @@
-// CONFIG ONLINE
-
 package config
 
 import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,15 +17,21 @@ var (
 	SQLDB *sql.DB
 )
 
-const (
-	host     = "1aseyi.h.filess.io"
-	port     = "3307"
-	user     = "tumbura_lifedream"
-	password = "35b858ae13a183abea8c340928a8f07c2bc63d8b"
-	dbname   = "tumbura_lifedream"
-)
-
 func Conn() {
+
+	// Load .env hanya jika ada (development lokal)
+	_ = godotenv.Load()
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	// Validasi agar mudah mendeteksi kesalahan konfigurasi
+	if host == "" || port == "" || user == "" || dbname == "" {
+		log.Fatal("Environment Variable database belum lengkap.")
+	}
 
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
@@ -38,7 +44,7 @@ func Conn() {
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Database connection failed: ", err)
 	}
 
 	sqlDB, err := db.DB()
@@ -46,40 +52,14 @@ func Conn() {
 		log.Fatal(err)
 	}
 
-	// Setting Connection Pool
-	sqlDB.SetMaxOpenConns(5)
-	sqlDB.SetMaxIdleConns(2)
-	sqlDB.SetConnMaxLifetime(30 * time.Minute)
-	sqlDB.SetConnMaxIdleTime(5 * time.Minute)
+	// Connection Pool
+	sqlDB.SetMaxOpenConns(20)
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetConnMaxLifetime(1 * time.Hour)
+	sqlDB.SetConnMaxIdleTime(30 * time.Minute)
 
 	DB = db
 	SQLDB = sqlDB
 
-	fmt.Println("Database Connected")
+	fmt.Println("✅ Database Connected")
 }
-
-// CONFIG LOCAL
-
-// package config
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"gorm.io/driver/mysql"
-// 	"gorm.io/gorm"
-// )
-
-// var DB *gorm.DB
-
-// func Conn() {
-// 	dsn := "root:@tcp(127.0.0.1:3306)/goland?parseTime=true"
-// 	var err error
-
-// 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		log.Fatal("Failed to connect to database: ", err)
-// 	} else {
-// 		fmt.Println("Database connection successful")
-// 	}
-// }
